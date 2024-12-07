56bda70148708ae1ed88bd51c835328872807504
Caio <caiofasoares@gmail.com>
2024-12-07 18:17:42 -0300

    Initial Commit

---
+++ edge/edgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  edgeApp.swift
+//  edge
+//
+//  Created by Caio Soares on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct edgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
